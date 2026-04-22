f9f7ef21404a72628954e8654db8c1112da74e09
Silvain <silvain.louis@iadys.com>
2026-04-22 09:38:20 +0200

    Compil change

--- components/JSY_MK_194T_decoder/JSY_MK_194T_decoder.h
+++ components/JSY_MK_194T_decoder/JSY_MK_194T_decoder.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
+#include <deque>
 
 namespace esphome {
 namespace JSY_MK_194T_decoder {
